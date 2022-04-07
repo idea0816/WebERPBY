48e55021ecfd856c14432119e3d3b6ae43c84bbc
CXY <idea0816@gmail.com>
2022-04-07 23:09:14 +0800

    初始化 WebERPBY

---
+++ src/main/java/com/cxy/weberpby/WebErpbyApplication.java
@@ -0,0 +1,13 @@
+package com.cxy.weberpby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebErpbyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebErpbyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cxy/weberpby/WebErpbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxy.weberpby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebErpbyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
